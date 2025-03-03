99d97e5d0e41e12390e69ee7403ccab44b5e2658
Roz Townsend <rdtownsend@protonmail.com>
2025-03-03 14:36:13 +0100

    assignment added

---
+++ aboutMe.js
@@ -0,0 +1,41 @@
+const displayAnswer = (answer) => {
+    console.log(answer);
+}
+
+const questionAndAnswerGenerator = async (displayAnswer) => {
+    const timer = (question, answer) => {
+        return new Promise(resolve => {
+            const questionData = question;
+            console.log(question);
+            setTimeout(() => {
+                const answerData = answer;
+                displayAnswer(answerData);
+                setTimeout(resolve, 1500);
+            }, 1000);
+        });
+    };
+
+
+    const myName = async () => {
+        await timer("Name:", "Roz Townsend");
+    };
+    
+    const myAge = async () => {
+        await timer("Age:", "41");
+    };
+
+    const myLocations = async () => {
+        await timer("Birth Location and Current Location:", "Tacoma Washington USA and Undenäs Sweden");
+    };
+
+    const myHobbies = async () => {
+        await timer("Hobbies:", "Drawing and permaculture")
+    };
+
+   await myName();
+   await myAge();
+   await myLocations();
+   await myHobbies();
+};
+
+questionAndAnswerGenerator(displayAnswer);
---
+++ dryAboutMe.js
@@ -0,0 +1,26 @@
+// I wanted to see if I could make things more succinct, but I know these aren't technically separate functions for each Q and A so it doesn't fulfill the requirements of the assignment.
+
+
+const bioData = {
+    name: {question: "What's my name?...", response: "It's Roz Townsend"},
+    age: {question: "What's my age?...", response: "41. Did you know your age can be used against you in discriminatory hiring practices?"},
+    locations: {question: "Where were you born? Where do you live now?...", response: "Tacoma Washington USA and Undenäs Sweden."},
+    hobbies: {question: "What are your hobbies?...", response: "Drawing and gardening."}
+}
+
+const displayData = (awaitedData) => {
+    console.log(awaitedData);
+};
+
+const questionAndAnswerGenerator = async (data, displayData) => {
+    for (let key in data) {
+        console.log(data[key].question);
+        await new Promise(resolve => setTimeout(resolve, 1000));
+            const myResponse = data[key].response;
+            displayData(myResponse);
+            await new Promise(resolve => setTimeout(resolve, 2000));
+        };
+    };
+
+questionAndAnswerGenerator(bioData, displayData);
+
